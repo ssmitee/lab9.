e7560bf2f2bd48de8909136bf6f42f8602802be3
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-27 22:48:26 +0200

    Добавьте файлы проекта.

---
+++ lab9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class MyClass
+{
+    private double field1;
+    private double field2;
+
+    public MyClass(double field1, double field2)
+    {
+        this.field1 = field1;
+        this.field2 = field2;
+    }
+
+    public string GetInfo()
+    {
+        return $"Поле 1: {field1}, Поле 2: {field2}";
+    }
+
+    public double ProcessFields()
+    {
+
+        return Math.Pow(field1, 2);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double field1 = double.Parse(Console.ReadLine());
+        double field2 = double.Parse(Console.ReadLine());
+
+        MyClass obj = new MyClass(field1, field2);
+
+        string info = obj.GetInfo();
+        double result = obj.ProcessFields();
+
+        Console.WriteLine(info);
+        Console.WriteLine(result);
+    }
+}
